af8b265d36f5be60afdea4c3cf4ba1b38171d95d
Arthur Vieira de Assis Moreira <arthurvieiram@icloud.com>
2022-10-25 18:38:42 -0300

    incrementando a pagina de login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./Login.css"
 
 function Login() {
 
     return (
         <div className = "base">
-            <div className = "container">
-                <div>
-                    <h1> Entrar </h1>
+                <div className = "login">
+                    <div className = "box">
                     <Form className = "inputs">
                         
+                        <h1 className = "entrar"> Entrar </h1>
+
                         <Form.Group className="mb-3" controlId="email">
                             <Form.Control type="email" placeholder="Endereço de email" />
                         </Form.Group>
@@ -19,18 +21,25 @@ function Login() {
                             <Form.Control type="password" placeholder="Senha" />
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Lembrar de mim" />
-                        </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <div className = "componentes">
+                                <Form.Check type="checkbox" label="Lembrar de mim" />
+                                <Link to="cadastro" className="esqueceusenha"> Esqueceu sua senha? </Link>
+                            </div>
+                            </Form.Group>
 
                         <Button variant="secondary"> Entrar </Button>
-
+                        
                     </Form>
+
+                    <Link to="cadastro" className="cadastrar"> Novo por aqui? Cadastre-se e assista a filmes agora! </Link>
+
+                    </div>
+
                 </div>
-        </div>
     </div>
     )
-    
+
 }
 
 export default Login;
