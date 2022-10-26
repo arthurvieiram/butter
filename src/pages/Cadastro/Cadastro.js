6bb660fa3a51efb8eb3b38a116b0ec3d396dcc28
Arthur Vieira de Assis Moreira <arthurvieiram@icloud.com>
2022-10-25 21:28:06 -0300

    pagina de cadastro finalizada

--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -1,76 +1,75 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import "./Cadastro.css"
 
 function Cadastro() {
-    
+
+    const[nome, setNome] = useState();
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const history = useHistory();
 
+    function cadastro() {
+        history.push("inicio")
+    }
+
     return (
         <div className = "base">
                 <div className = "cadastro">
+                    
                     <div className = "box">
+                    
+                    <h1 className = "cadastro"> Cadastro </h1>
+                    
                     <Form className = "inputs">
-                        
-                        <h1 className = "cadastrar"> Cadastrar </h1>
-
-                        <h6 className = "dados_gerais"> Dados dados gerais </h6>
+                        <h6 className = "dadosgerais"> Dados gerais </h6>
 
                         <Form.Group className="mb-3" controlId="nome">
                             <Form.Control 
                             type="nome" 
                             placeholder="Nome completo" 
-                            />
+                            onChange = {(e) => setNome(e.target.value)} />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="email">
                             <Form.Control 
                             type="email" 
                             placeholder="Endereço de email" 
-                            />
+                            onChange = {(e) => setEmail(e.target.value)} />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="senha">
                             <Form.Control 
                             type="password" 
                             placeholder="Senha" 
-                            />
+                            onChange = {(e) => setPassword(e.target.value)}/>
                         </Form.Group>
 
                         <h6 className = "endereço"> Endereço </h6>
 
+                        <Form.Group className="mb-3" controlId="pais">
+                            <Form.Control 
+                            type="pais" 
+                            placeholder="País" 
+                            />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="uf">
+                            <Form.Control 
+                            type="uf" 
+                            placeholder="Estado" 
+                            />
+                        </Form.Group>
+
                         <Form.Group className="mb-3" controlId="endereço">
                             <Form.Control 
                             type="endereço" 
-                            placeholder="Endereço" 
+                            placeholder="Endereço completo" 
                             />
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="ue">
-                            <Form.Control 
-                            type="ue" 
-                            placeholder="UE " 
-                            />
-                        </Form.Group>
-
-                         <Form.Group className="mb-3" controlId="pais">
-                            <Form.Control 
-                            type="pais" 
-                            placeholder="País " 
-                            />
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="complemento">
-                            <Form.Control 
-                            type="complemento" 
-                            placeholder="Complemento" 
-                            />
-                        </Form.Group>
-
-                        <Button variant="secondary" onClick = {(Cadastro)}> Cadastrar </Button>
+                        <Button variant="secondary" onClick = {(cadastro)}> Cadastrar </Button>
                         
                     </Form>
 
