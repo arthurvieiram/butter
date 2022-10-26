ab0f3ce0dba0acad8c5426d79c9d0fe2b4c3b3e3
Arthur Vieira de Assis Moreira <arthurvieiram@icloud.com>
2022-10-26 17:04:21 -0300

    inicio do desenvolvimento da pagina home

--- src/pages/Inicio/Inicio.js
+++ src/pages/Inicio/Inicio.js
@@ -5,7 +5,19 @@ function Inicio() {
 
     return (
         <div className="baseInicio">
-            <h1 className="tituloInicio"> Inicio </h1>
+            <div className="textosInicio">
+                
+                <h1 className="tituloInicio"> Inicio </h1>
+                <h4 className="texto1"> Temos o compromisso de entregar qualidade ao manter o profissionalismo em um trabalho mais eficiente! </h4>
+                <h5 className="texto2"> A Butter é uma empresa de universitários que estão em busca de colocar seus conhecimentos em prática, visando a entrega de um serviço de streaming excepcional. </h5>
+            </div>
+            
+            <h1 className="tituloMaisAssistidos"> Filmes mais assistidos </h1>
+            <div className ="maisAssistidos">
+                    <div className="boxMA1"></div>
+                    <div className="boxMA2"></div>
+                    <div className="boxMA3"></div>
+            </div>
         </div>
     )
 }
