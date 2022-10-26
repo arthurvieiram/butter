45686d6efc718fa2e3aec66bb814626e5ed6314a
Jean Lopes <pierry.jeann@gmail.com>
2022-10-26 15:39:29 -0300

    Início filmes

--- src/pages/Filmes/Filmes.js
+++ src/pages/Filmes/Filmes.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import "./Filmes.css"
 function Filmes() {
     return (
         <div>
-            <h1> Filmes </h1>
+            <h1> Filmes Favoritados </h1>
         </div>
     )
 }
